b77ed939f1088b806ef57eaad2767b46e751b310
Maria Malyarova <savoreux69@gmail.com>
2016-03-30 12:54:39 +0300

    introduced the possibility of conversion to UTF - 8

--- frequency analysis/task.py
+++ frequency analysis/task.py
@@ -1,8 +1,9 @@
+import simplejson
 file = open('text.txt','r')
 stroka = file.read()
 str = 'абвгдеёжзиклмнопрстуфхцчшщъыьэюя'
 dict = {}
-
+stroka = stroka.lower()
 for line in stroka:
     for i in line:
         if str.find(i) > -1:
@@ -10,5 +11,11 @@ for line in stroka:
                 dict[i] += 1
             else:
                 dict[i] = 0
+
 l = lambda x: x[1]
-print(sorted(dict.items(), key=l, reverse = True))
+ans = sorted(dict.items(), key=l, reverse = True)
+print(ans)
+f = open('itog.txt', 'w')
+simplejson.dump(ans, f, ensure_ascii = False)
+f.close()
+

e51161f8a4562fb0b3554f9fa9e38e569b083e35
Maria Malyarova <savoreux69@gmail.com>
2016-05-24 14:34:30 +0300

    some changes

--- check field F /shifr (may be).py
+++ check field F /shifr (may be).py
@@ -1,24 +1,29 @@
-file = open('matr.txt', 'r')
-str_message = file.read()
+file = open('text.txt', 'r')
+str1 = file.read()
 
-a = str_message.split()
+a = str1.split()
 a = [int(x) for x in a]
+
 matr = []
-summa = 0
+kol = 0
+check = False
+null = 0
+ed = 1
+type = True
 
 while a != []:
     matr1 = a[:9]
     matr.append(matr1)
     a = a[9:]
-    summa += 1
+    kol += 1
 
-def additivus(matr1, matr2):
+def add(matr1, matr2): # сложение по модулю 2
         matr3 = []
         for i in range(len(matr1)):
             matr3.append((matr1[i] + matr2[i]) % 2)
         return matr3
 
-def multiplex(matr1, matr2):
+def mul(matr1, matr2):
     matr3 = []
     for j in range(3):
         for i in range(3):
@@ -26,130 +31,161 @@ def multiplex(matr1, matr2):
             matr22 = matr2[i::3]
             s = 0
             for i in range(len(matr22)):
-                      s+= (matr11[i]*matr22[i])
-            matr3.append(s%2)
+                      s += (matr11[i] * matr22[i])
+            matr3.append(s % 2)
     return matr3
 
-for i in range(summa):
-    for j in range(summa):
-        if additivus(matr[j],matr[i]) == matr[j]:
+for i in range(kol): # нахождение нулевой матрицы (при сложении получается нулевая)
+    for j in range(kol):
+        if add(matr[j],matr[i]) == matr[j]:
             check = True
             print('Нулевой элемент найден: ')
             q = matr[j]
-            print(q[0], q[1], q[2])
-            print(q[3], q[4], q[5])
-            print(q[6], q[7], q[8])
+            print(q[0],q[1], q[2])
+            print(q[3],q[4],q[5])
+            print(q[6],q[7], q[8])
             null = q
             break
-
 if not(check):
-    print('Нулевой элемент не найден')
-else:
-    check = False
-    for i in range(summa):
-        for j in range(summa):
-            if multiplex(matr[j],matr[i]) == matr[j] and matr[j] != null:
+     print('Нулевой элемент не найден')
+     type = False
+if 1 > 0:
+    check = False # нахождение единичного элемента (при умножении на себя дает себя)
+    for i in range(kol):
+        for j in range(kol):
+            if mul(matr[j], matr[i]) == matr[j] and matr[j] != null:
                 check = True
                 print('Единичный элемент найден: ')
-                q = matr[j]
-                print(q[0], q[1], q[2])
-                print(q[3], q[4], q[5])
-                print(q[6], q[7], q[8])
+                q = matr[i]
+                print(q[0],q[1],q[2])
+                print(q[3],q[4],q[5])
+                print(q[6],q[7],q[8])
                 ed = q
                 break
-
     if not(check):
         print('Единичный элемент не найден')
-    check = False
-    for i in range(summa):
-        for j in range(summa):
-            if additivus(matr[j], matr[i]) != additivus(matr[i], matr[j]):
+        type = False
+
+if 1 > 0:
+    check = False # проверка коммутативности
+    # a + b = b + a?
+    for i in range(kol):
+        for j in range(kol):
+            if add(matr[j], matr[i]) != add(matr[i], matr[j]):
                 check = True
                 break
     if check:
-        print('Коммутативность сложения НЕ выполняется')
+        print('Коммутативность сложения не выполняется')
+        type = False
     else:
         print('Коммутативность сложения выполняется')
 
-    h = 0
-
-    for i in range(summa):
+    h = 0 # определения противоположных элементов
+    # если сумма 2х элементов равна нулевому элементу
+    for i in range(kol):
         check = False
-
         if matr[i] != null:
-            for j in range(summa):
-                if additivus(matr[j], matr[i]) == null:
+            for j in range(kol):
+                if add(matr[j], matr[i]) == null:
                     check = True
                     break
             if not(check):
-                print('Не найдены противоположные элементы')
+                print('Противоположные элементы не найдены')
                 print(matr[i])
                 h = 1
+                type = False
     if h != 1:
-        print('Найдены все противоположные элементы')
+        print('Противоположные элементы существуют')
 
-    if ed != 1:
+    # определение существования обратного элемента для всех остальных элементов
+    if ed != 1: # если есть единичный элемент
         s = 0
-        for i in range(summa):
+        for i in range(kol):
             if matr[i] != null:
                 check = False
-                for j in range(summa):
-                    if multiplex(matr[i],matr[j]) == ed:
+                for j in range(kol):
+                    if mul(matr[i], matr[j]) == ed: # если произведение двух элементов дает единичный элемент
                         check = True
                         break
                 if not(check):
-                    print('НЕ найдены обратные элементы для некотрого элемента')
+                    print('Не найдены обратные элементы для некотрого элемента')
                     print(matr[i])
                     s = 1
+                    type = False
         if s != 1:
-            print('Найдены обратные элементы для всех')
+            print('Обратные элементы найдены')
         else:
-            print('Единчиный элемент не найден! Обратные элементы искать не можем')
+            print('Не для всех элементов найдены обратные')
+            type = False
     else:
-     print('Нулевого элемента нет! Проверять некторые условия не можем')
+     print('Единичного элемента не существует, невозможно найти обратные элементы')
+     type = False
 
-check = False
-for i in range(summa):
-    for j in range(summa):
-        for k in range(summa):
-            if additivus(additivus(matr[j],matr[i]),matr[k]) != additivus(matr[i],additivus(matr[j],matr[k])):
+check = False # нахождение ассоциативности сложения
+# (a + b) + c = a + (b + c)?
+for i in range(kol):
+    for j in range(kol):
+        for k in range(kol):
+            if add(add(matr[i], matr[j]), matr[k]) != add(matr[i], add(matr[j], matr[k])):
                 check = True
                 break
 if check:
-    print('Ассоциативность сложения НЕ выполняется')
+    print('Ассоциативность сложения не выполняется')
+    type = False
 else:
     print('Ассоциативность сложения выполняется')
 
-check = False
-for i in range(summa):
-    for j in range(summa):
-        if multiplex(matr[j],matr[i]) != multiplex(matr[i],matr[j]):
+check = False # нахождение коммутативности умножения
+# ab = ba?
+for i in range(kol):
+    for j in range(kol):
+        if mul(matr[j], matr[i]) != mul(matr[i], matr[j]):
             check = True
             break
 if check:
-    print('Коммутативность умножения НЕ выполняется')
+    print('Коммутативность умножения не выполняется')
+    type = False
 else:
     print('Коммутативность умножения выполняется')
-check = False
-for i in range(summa):
-    for j in range(summa):
-        for k in range(summa):
-            if multiplex(multiplex(matr[j],matr[i]),matr[k]) != multiplex(matr[i],multiplex(matr[j],matr[k])):
+
+check = False # определение ассоциативности умножения
+# (a * b) * c = a * (b * c)?
+for i in range(kol):
+    for j in range(kol):
+        for k in range(kol):
+            if mul(mul(matr[i], matr[j]), matr[k]) != mul(matr[i], mul(matr[j], matr[k])):
                 check = True
                 break
 if check:
-    print('Ассоциативность умножения НЕ выполняется')
+    print('Ассоциативность умножения не выполняется')
+    type = False
 else:
     print('Ассоциативность умножения выполняется')
 
-check = False
-for i in range(summa):
-    for j in range(summa):
-        for k in range(summa):
-            if multiplex(additivus(matr[i],matr[j]),matr[k]) != additivus(multiplex(matr[i],matr[k]),multiplex(matr[j],matr[k])):
+check = False # определение дистрибутивности умножения относительно сложения
+# (a + b) * c = a * c + b * c?
+for i in range(kol):
+    for j in range(kol):
+        for k in range(kol):
+            if mul(add(matr[i],matr[j]),matr[k]) != add(mul(matr[i],matr[k]),mul(matr[j],matr[k])):
                 check = True
                 break
 if check:
-    print('Дистрибутивность умножения относительно сложения НЕ выполняется')
+    print('Дистрибутивность умножения относительно сложения не выполняется')
+    type = False
 else:
     print('Дистрибутивность умножения относительно сложения выполняется')
+
+if type:
+    for i in matr:
+        w = []
+        for j in range(2):
+            w = add(i, i)
+        if w == i:
+            print('Является полем характеристики 2')
+            exit(0)
+        else:
+            print('Не является полем характеристики 2')
+            exit(0)
+else:
+    print('Не является полем')
